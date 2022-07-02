426d796d47144016052e3c9acb870aa70080860e
LukasP <lukas.straubing@gmail.com>
2022-07-02 13:01:39 +0200

    add user to db dto created

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AddUserDto } from './dtos/addUser.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+  @Post()
+  async addUser(@Body() body: AddUserDto) {
+    return await this.userService.addUser(body);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
 import { Repository } from 'typeorm';
+import { v4 } from 'uuid';
 
 @Injectable()
 export class UserService {
@@ -9,4 +10,13 @@ export class UserService {
     @InjectRepository(UserEntity)
     private userEntityRepository: Repository<UserEntity>,
   ) {}
+
+  async addUser(userObj): Promise<string> {
+    const user = userObj;
+    user.userID = v4();
+    user.numberOfWalks = 0;
+    user.isAdmin = 0;
+    await this.userEntityRepository.save(user);
+    return user.userID;
+  }
 }
