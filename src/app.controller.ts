b5f994f7a4518239b7ab5b3833f29f4a1ec9dd75
LukasP <lukas.straubing@gmail.com>
2022-07-12 09:28:03 +0200

    change jwt response, now set cookie http only with jwt, jwt strategy changed to read jwt token from cookie

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Request, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { Public } from './decorators/public-decorator';
@@ -16,9 +16,15 @@ export class AppController {
   @Public()
   @UseGuards(LocalAuthGuard)
   @Post('signIn')
-  async login(@Request() req) {
+  async login(@Request() req, @Res() res) {
     try {
-      return this.authService.login(req.user);
+      const jwt = this.authService.login(req.user);
+      res.cookie('jwt', jwt, {
+        httpOnly: true,
+        secure: true,
+        maxAge: 6 * 60 * 1000,
+      });
+      res.json(jwt);
     } catch (err) {
       console.log(err);
     }
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,14 +2,28 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { jwtAccess } from '../config/authConfig';
+import { Request } from '@nestjs/common';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: jwtAccess,
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        // @ts-ignore
+        (request: Request) => {
+          // @ts-ignore
+          const data = request?.cookies['jwt'];
+          // @ts-ignore
+          console.log(request.cookies);
+          console.log(data);
+          if (!data) {
+            return null;
+          }
+          return data;
+        },
+      ]),
     });
   }
 
